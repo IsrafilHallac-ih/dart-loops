ad2adfb801a4ddd07e63cc7802be680c1b410b14
İSRAFİL HALLAÇ <83336393+IsrafilHallac-ih@users.noreply.github.com>
2022-04-05 16:27:32 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main() {
+  for (var i = 2; i <= 20; i += 2) {
+    print(i);
+  }
+
+  var lessons = ["dart", "flutter", "java", "c#"];
+  for (var lesson in lessons) {
+    print(lesson);
+  }
+
+  var number = 10;
+  while (number <= 20) {
+    print(number);
+    number++;
+  }
+}
